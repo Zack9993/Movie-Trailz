d36cdfcaec26cdf9ecb20fd4aa8bba5644dfc1b8
happy.c.much <happy.c.much@gmail.com>
2024-09-09 18:01:50 +0000

    main apichecker movie tv homepage

---
+++ lib/apikey/apikey.dart
@@ -0,0 +1,2 @@
+//reanme this file extension to dart from .dart
+String apikey ="place your api key";
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,89 @@
-import 'package:flutter/material.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sp = await SharedPreferences.getInstance();
+  String imagepath = sp.getString('imagepath') ?? '';
+  runApp(MyApp(
+    imagepath: imagepath,
+  ));
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+  //     overlays: [SystemUiOverlay.bottom]);
+}
+
+class MyApp extends StatelessWidget {
+  String imagepath;
+  MyApp({
+    super.key,
+    required this.imagepath,
+  });
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: intermediatescreen(),
+    );
+  }
+}
+
+class intermediatescreen extends StatefulWidget {
+  const intermediatescreen({super.key});
+
+  @override
+  State<intermediatescreen> createState() => _intermediatescreenState();
+}
+
+class _intermediatescreenState extends State<intermediatescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      // disableNavigation: true,
+      backgroundColor: Color.fromRGBO(18, 18, 18, 1),
+
+      duration: 2000,
+      nextScreen: MyHomePage(),
+      splash: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('asset/icon.png'),
+                          fit: BoxFit.contain)),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Text(
+                    'By Niranjan dahal',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      // splash: Image.asset('assets/images/background.jpg'),
+      splashTransition: SplashTransition.fadeTransition,
+      splashIconSize: 200,
+      // centered: false,
+    );
+  }
+}import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
